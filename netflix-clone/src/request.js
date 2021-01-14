a1fcecb68171fa5f87011ce9bc8f6045011d7320
Nguyen Duc Duy <nguyenducduy1707@gmail.com>
2021-01-14 21:28:07 +0700

    First commit

---
+++ netflix-clone/src/request.js
@@ -0,0 +1,13 @@
+const APIKEY = "092633b352053063174fc5e81bc636f0";
+
+const request = {
+    fetchTrending : `/trending/all/week?api_key=${APIKEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${APIKEY}&with_network=213`,
+    fetchTopRated : `/movie/top_rated?api_key=${APIKEY}&language=en_US`,
+    fetchActionMovies : `/discover/movie?api_key=${APIKEY}&with_genres=28`,
+    fetchComedyMovies : `/discover/movie?api_key=${APIKEY}&with_genres=35`,
+    fetchHorrorMovies : `/discover/movie?api_key=${APIKEY}&with_genres=27`,
+    fetchRomanceMovies : `/discover/movie?api_key=${APIKEY}&with_genres=10749`,
+    fetchDocumentaries : `/discover/movie?api_key=${APIKEY}&with_genres=99`,
+}
+export default request;
