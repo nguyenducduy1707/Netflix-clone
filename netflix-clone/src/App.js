ec1ff614e60a8f2d40fbb94d9d119a45de5772f5
Nguyen Duc Duy <nguyenducduy1707@gmail.com>
2021-01-15 00:13:26 +0700

    Create rows and style them

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,8 +1,25 @@
 import React from "react"
 import "./App.css"
+import request from "./request";
+import Row from "./Row"
+
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      {/* Nav */}
+      {/* Banner */}
+      <Row 
+        title="NETFLIX ORIGINAL" 
+        fetchUrl={request.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending now" fetchUrl={request.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={request.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={request.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies}/>
+      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={request.fetchDocumentaries}/>
     </div>
   );
 }
---
+++ netflix-clone/src/Row.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import axios from "./axios" ;
+import "./Row.css"
+const base_url = "https://image.tmdb.org/t/p/original/"
+
+function Row(props) {
+    const {title, fetchUrl, isLargeRow} = props ;
+    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        // run once when the rows load : []
+        async function fetchData() {
+            const request = await axios.get(fetchUrl) ;
+            setMovies(request.data.results)
+            return request
+        }
+        fetchData()
+    }, [fetchUrl])
+    console.log(movies)
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+            <div className={"row_posters"}>
+                {movies.map((movie) => {
+                    return <img
+                    key={`${base_url}${movie.id}`}
+                    className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+                    src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
+                    alt={movie.name}></img>
+                })}
+            </div>
+        </div>
+    );
+}
+
+export default Row;
